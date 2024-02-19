8a00f9e84dd9347498e75e96217d9ec84215ddbb
bhuvanaa111 <bhuvanakarnati89@gmail.com>
2024-02-19 18:57:00 +0000

    initial commit

---
+++ list-users.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+########################
+# About:
+# Input:
+#
+# Owner:
+##########################
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+# Function to check the number of command-line arguments
+function helper {
+    expected_cmd_args=4
+    if [ "$#" -ne "$expected_cmd_args" ]; then
+        echo "Please execute the script with the required command-line arguments."
+        echo "Usage: ./list-users.sh <username> <token> <repo_owner> <repo_name>"
+        exit 1
+    fi
+}
+
+# Main script
+helper "$@"  # Call the helper function with command-line arguments
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
